f10f6520fcffaa9d3bf6a84835ef1f3936da608f
Alexander Benedict <abenedi1@binghamton.edu>
2016-12-01 14:50:37 -0500

    Added base gui

---
+++ project/ViewsOrganizer.java
@@ -0,0 +1,20 @@
+package project;
+
+public class ViewsOrganizer {
+	private MachineModel model;
+	public MachineModel getModel() {
+		return model;
+	}
+	public void setModel(MachineModel model) {
+		this.model = model;
+	}
+	public void step(){
+		
+	}
+	public void cleatJob(){
+		
+	}
+	public void makeReady(String s){
+		
+	}
+}
